c83f14f00deeb9ab7f3f550d95120842d71afda5
Patrick Dunn <patrick@theonevehicle.com>
2019-12-07 22:32:50 +1100

    Implemented javascript to change product image src on product show page by clicking on color code

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,10 +1,21 @@
-console.log("Hello");
 
-function doSomething() {
-    alert("hello");
-}
+const MAIN_IMAGE_URL = 'http://via.placeholder.com/800x200?text=';
 
+// Function: change main image src on product show view
+function changeImage (imageText) {
+  productImage = document.getElementById('product-image');
+  productImage.setAttribute('src', MAIN_IMAGE_URL + imageText);
+};
+
+// Await page load before running js code
 document.addEventListener('turbolinks:load', () => {
-  var myId = document.getElementById("my-id");
-  myId.addEventListener("click", doSomething)
+  // Get dynamically created product color elements
+  var productColors = document.getElementsByClassName('product-color');
+  // Assign click listener to dynamically created product color elements. 
+  // Element names equate to the product code e.g. BL107-A
+  for (let i = 0; i < productColors.length; i++ ) {
+    productColors[i].addEventListener('click', function () {
+      changeImage(productColors[i].id);
+    });
+  };
 });
